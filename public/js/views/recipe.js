860d4a674716f69e94b94008dd2c69d4689ebf96
Jon Culver <jonathanhculver@gmail.com>
2015-08-23 18:53:30 -0400

    creating templates directory

--- public/js/views/recipe.js
+++
@@ -1,9 +0,0 @@
-// <script type="text/x-handlebars-template" id="recipe">
-// 	<div class="card_container">
-// 		<img class="recipeImg" />
-// 		<div class="card">
-// 			<div class="title">{{ title }}</div>
-// 			<div class="description">{{ description }}</div>
-// 		</div>
-// 	</div>
-// </script>
