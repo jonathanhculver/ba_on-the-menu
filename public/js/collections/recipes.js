e9e27ea9d518f044b6d3f15892c4f414570c64e7
Jon Culver <jonathanhculver@gmail.com>
2015-08-26 21:23:17 -0400

    fixing backend, on fetch success render recipes

--- public/js/collections/recipes.js
+++ public/js/collections/recipes.js
@@ -4,33 +4,6 @@ var app = app || {};
 	app.RecipeCollection = Backbone.Collection.extend({
 		model: app.Recipe,
 		url: '/api/menu',
-		// parse: function(response) {
-
-		// },
-
-		familyPlan: function() {
-			var result = [];
-
-			this.models.forEach(function(value){
-				value.get('family_plan').recipes.forEach(function(recipe){
-					result.push(recipe.recipe);
-				});
-			});
-
-			return new app.RecipeCollection(result);
-		},
-
-		couplePlan: function() {
-			var result = [];
-
-			this.models.forEach(function(value){
-				value.get('two_person_plan').recipes.forEach(function(recipe){
-					result.push(recipe.recipe);
-				});
-			});
-
-			return new app.RecipeCollection(result);
-		},
 
 		getWeeks: function() {
 			var result = [];
--- public/js/views/menu.js
+++ public/js/views/menu.js
@@ -13,30 +13,31 @@ var app = app || {};
 
 		constructor: function() {
 			Backbone.Viewmaster.prototype.constructor.apply(this, arguments);
+			var self = this;
 
 			// create collection of recipes, pulled from the api
 			this.collection = new app.RecipeCollection();
-			this.collection.fetch();
+			this.collection.fetch({
+				success: function(){
+					self.renderRecipes('two_person_plan', 0);
+				}
+			});
 
 			//reset collection to the default recipes on load
-			this.listenTo(this.collection, "add", function(model){
-				this.appendView(".menuContainer", new app.RecipeContainerView({
-					collection: model.getRecipes('two_person_plan'),
-					model: model
-				}));
+			this.listenTo(this.collection, "add", function(model, collection, options){
+				//append dropdown view
 				this.render();
 			});
-			//render the recipe views
-			// this.listenTo(this.collection, "reset", function(collection){
-			// 	for(var i = 0; i< 2; i++) {
-			// 		this.appendView(".menuContainer", new app.RecipeRowView({
-			// 			collection: collection,
-			// 			index: i
-			// 		}));
-			// 	}
-			// });
 
 			this.setView(".header", new app.HeaderView());
+		},
+
+		renderRecipes: function(plan, index) {
+			this.setView(".menuContainer", new app.RecipeContainerView({
+				collection: this.collection.models[index].getRecipes(plan),
+				model: this.collection.models[index]
+			}));	
+			this.render();
 		}
 	});
 })();
